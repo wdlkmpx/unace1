4db1f3879aed9da0fa5478c79193c1257fd62d49
wdlkmpx <wdlkmpx@gmail.com>
2021-05-26 18:55:03 -0500

    [debian] 003_security.patch

--- uac_crt.h
+++ uac_crt.h
@@ -8,7 +8,7 @@
 
 #include "acestruc.h"
 
-CHAR *ace_fname(CHAR * s, thead * head, INT nopath);
+CHAR *ace_fname(CHAR * s, thead * head, INT nopath, unsigned int size);
 INT  create_dest_file(CHAR * file, INT a);
 
 #ifdef UNIX
